5a5b51b8ce30c49172aea3c3a50615294094d1be
Mohamad <mohammadbarakat342@gmail.com>
2019-05-10 12:39:01 +0300

    Sync Coditech / Submission master

---
+++ Challenge/script##.js
@@ -0,0 +1,6 @@
+var a =  10;
+alert(a);
+var x = prompt("enter first number");
+var y = prompt("enter second number");
+alert(true = x==y);
+alert(false = x!=y);
---
+++ Challenge/script.js
@@ -0,0 +1,5 @@
+
+var y = prompt("enter y");
+var x = prompt ("enter x");
+alert(true = x==y);
+alert(false = x!=y);
