108c4a2ef1da152a46f4a3c2c3ba2cc7836480c0
Mohamad <mohammadbarakat342@gmail.com>
2019-05-20 11:49:53 +0300

    Strings concatenation 2

---
+++ Exercises/Javascript-Basics-01/step_4.js
@@ -0,0 +1,4 @@
+var name = prompt("Enter Your Name");
+var surName = prompt ("Enter Surname");
+var city = prompt ("Enter Your City");
+alert("Name:"+ " " + name +  "\n" +"Surname:" + " " + surName + "\n" + "City:" + " "  + city);
