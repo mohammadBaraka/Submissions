0c4c99366988f52ba127bfaf2211aae124d2945d
Mohamad <mohammadbarakat342@gmail.com>
2019-05-23 14:08:10 +0300

    step_4

---
+++ Exercises/Javascript-Basics-02/step 4/step_4.js
@@ -0,0 +1,12 @@
+var b =document.querySelector("button").addEventListener("click",myfunction);
+var a = document.querySelector("input");
+
+function myfunction(){
+    var r =confirm;
+    if(r ("yes or no")){
+        r = document.getElementById("name").value = " ";
+            document.getElementById("surname").value = " ";
+            document.getElementById("city").value = " ";
+    }
+    
+}
