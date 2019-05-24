b13568b662dcca2171e0352683054e2f1f871963
Mohamad <mohammadbarakat342@gmail.com>
2019-05-24 15:58:53 +0300

    step_4

---
+++ Exercises/Javascript-Basics-03/step 4/step_4.js
@@ -0,0 +1,14 @@
+var a = document.querySelectorAll("input");
+a[0] = document.getElementById("password");
+a[1] = document.getElementById("confirmation");
+
+var b = document.querySelector("button").addEventListener("click", myfunction);
+function myfunction(){
+    if(a[0].value!=a[1].value){
+        a[0].style.borderColor = "red";
+        a[1].style.borderColor = "red";
+        }
+        else {
+            console.log ("text");
+        }
+}
