49aee6b6a90084836d886651c0b5299b91342832
Mohamad <mohammadbarakat342@gmail.com>
2019-05-24 15:03:20 +0300

    step_2

---
+++ Exercises/Javascript-Basics-03/step 2/step_2.js
@@ -0,0 +1,13 @@
+var y = document.querySelector("p");
+var x = document.querySelectorAll("a");
+x[0] = document.getElementById("show").addEventListener("click",function(){myfunction(false)});
+x[1] = document.getElementById("hide").addEventListener("click",function(){myfunction(true)});
+
+function myfunction(isHidden){
+    if (!isHidden){
+        y.style.display = "block";
+    }
+    else if (isHidden){
+        y.style.display = "none";
+}
+}
