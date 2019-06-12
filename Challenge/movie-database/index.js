d7ea4aa8a2f82842626f6b14d5eca08df29634f3
Mohamad <mohammadbarakat342@gmail.com>
2019-06-12 16:07:19 +0300

    Create an express simple API

--- Challenge/movie-database/index.js
+++ Challenge/movie-database/index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => res.send('ok'))
-
+app.get('/', (req, res) => {res.send('ok')})
+app.get('/test',(req,res)=>{res.send({status:200, message:"ok"})})
+app.get('/time',(req,res)=>{res.send({status:200, message:new Date().getHours()+':'+new Date().getSeconds()})})
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
