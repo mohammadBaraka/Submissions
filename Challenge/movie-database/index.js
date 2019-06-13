763675b4f8c6593a8bd7aae0e68b755d6af3041c
Mohamad <mohammadbarakat342@gmail.com>
2019-06-13 14:30:25 +0300

    READ ONE

--- Challenge/movie-database/index.js
+++ Challenge/movie-database/index.js
@@ -1,4 +1,4 @@
-  
+
     const express = require('express')
     const app = express()
     const port = 3000
@@ -15,13 +15,13 @@
     if(req.query.s !== undefined && req.query.s !== ""){
     res.send({status:200, message:"ok", data:req.query.s})
     }
-     else {
+    else {
     res.send({status:500, error:true, message:"you have to provide a search"}) } } );
-   
-   
+
+
     app.get('/movies/create',(req,res)=>{})
     app.get('/movies/read',(req,res)=>{res.send({status:200, data: movie})})
-    
+
     app.get('/movies/read/by-date', (req,res)=>{  res.send({status:200, data:movie.sort(function(a,b) {
     return a.year - b.year;
     })
@@ -52,3 +52,11 @@
     { title: 'Brazil', year: 1985, rating: 8 },
     { title: 'الإرهاب والكباب‎', year: 1992, rating: 6.2 }
     ]
+    app.get('/movies/read/id/:id?',(req,res)=>{
+    if(req.params.id >=0 && req.params.id < movies.length){
+    res.send({status:200, data: movies[req.params.id]})
+    }
+    else{
+    res.send({status:404, error:true, message:`the movie ${req.params.id } does not exist`})
+    }
+    })
